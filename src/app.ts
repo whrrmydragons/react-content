03a2fa35ef736046ec80d1e3379626323821f0c1
omri haber <omrihaber@me.com>
2018-03-19 00:45:51 +0200

    halvaScript

--- src/app.ts
+++ src/app.ts
@@ -2,26 +2,79 @@ import { GraphQLServer } from 'graphql-yoga';
 //import express from 'express'
 //import bodyParser from 'body-parser'
 import {makeExecutableSchema} from 'graphql-tools';
-import * as mongoose from 'mongoose';
+import {connect} from 'mongoose';
 
-import typeDefs from './schema';
-import resolvers from './resolvers/resolvers';
-
-export const schema = makeExecutableSchema({
-    typeDefs,
-    resolvers
-})
-
-const mongoClient = mongoose.connect('mongodb://dbuser:dbpassword@ds111319.mlab.com:11319/typescript-test');
+// TODO define bluebird promises like in microsoft`s example
 
 
-const server = new GraphQLServer({
-    typeDefs,
-    resolvers,
-    context: req=>({
-        ...req,
-        
-    })
-})
+//import typeDefs from './schema';
+//import resolvers from './resolvers/resolvers';
 
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+//export const schema = makeExecutableSchema({
+//    typeDefs,
+//    resolvers
+
+const mongoClient = connect('mongodb://dbuser:dbpassword@ds111319.mlab.com:11319/typescript-test',
+(err)=>{
+    if(err)console.log("error connecting to mongo");
+    else console.log("connected to mongo");
+});
+
+
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+    components():[Component]
+    component(id:ID!):Component
+    pages():[Page]
+    page(id:ID!):Page
+    staticPages():[StaticPage]
+    staticPage(id:ID!):StaticPage
+    templates():[Template]
+    template(id:ID!):template
+  }
+
+  type Component{
+      _id:String!
+      name:String!
+      component:String!
+      data:String!
+      props:[String]
+  }
+
+  type Page{
+      _id:String!
+      title:String!
+      staticPage:StaticPage!
+      content:[]
+  }
+
+  type Assets{
+      backgroundImageHref:String
+      backgroundImageBase64:String
+      logoHref:String
+      logoBase64:String
+  }
+
+  type StaticPage{
+      _id:String!
+      name:String!
+      assets:Assets
+      template:[]
+  }
+  type Template{
+      _id:String
+      template:String
+      name:String
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: (_:any, { name }:any) => `Hello ${name || 'World'}`,
+  },
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers })
+server.start(() => console.log('Server is running on localhost:4000'))
