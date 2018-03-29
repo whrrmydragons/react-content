fa711b02789caf124ec5ec4b88d1d9720da5d8ff
OMRI <omri@Omris-MacBook-Pro.local>
2018-03-29 10:45:35 +0300

    added npm build

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ const { GraphQLServer } = require('graphql-yoga');
 
 const {resolvers,typeDefs} = require('./schema/schema.graphql');
 
+
 //TODO move to another file
 import  mongoose from 'mongoose';
 mongoose.set('debug',true);
@@ -11,21 +12,21 @@ mongoose.connect("mongodb://dbuser:dbpassword@ds261118.mlab.com:61118/react-cont
 export const Schema = mongoose.Schema;
 export {mongoose as mongoose};
 
-/*StaticPage.create({
-    name:"a name",
-    assets:{
-        backgroundImage:"something",
-        mainIcon:"Something",
-        footerIcon:"SomethingEsle",
-        otherImages:["a","b"]
-    },
-    content:["5aa4cafcf36d28237f19be88","5aa4cafcf36d28237f19be88"],
-
-});*/
 
 
 const options = { port: 4000 };
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 
+
+
+
 server.start(options, () => console.log('Server is running on localhost:' + options.port))
+
+/**
+ * myMethod
+ * TODO:add mongoose validators
+ * ! important
+ * ? question 
+ * @param parameter
+ */
--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,4 +1,6 @@
 import { expect } from 'chai';
+import graphql from 'graphql';
+
 
 import { hello } from '../src/server'
 
